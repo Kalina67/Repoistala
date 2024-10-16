8a9086790e1f4572706638119e3073e045fc7ddd
Aref Abou Trabi <aref.aboutrabi7@gmail.com>
2024-10-16 17:43:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//this is the JavaScript File that u should read and try to understand
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting normally
 
