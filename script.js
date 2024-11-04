eb9ef52d5f95b86fc308eb9b1a264a359cbf82ac
Aref Abou Trabi <aref.aboutrabi7@gmail.com>
2024-11-04 10:17:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
-    // Get the values
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
