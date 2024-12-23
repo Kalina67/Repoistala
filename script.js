5d603ec02559ebe52ef8737c689ee67b74d303e3
Aref Abou Trabi <aref.aboutrabi7@gmail.com>
2024-12-23 19:48:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //this is the JavaScript File that u should read and try to understand
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
-    //Get the Val
+    //Get the Va
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
